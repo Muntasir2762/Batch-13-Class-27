d41fefcd03423bc853b6bf1d01d7786afd6caa42
Munotasir2762 <muntasirpranto2762@gmail.com>
2024-01-11 00:07:54 +0600

    Array in Javascript

---
+++ script.js
@@ -0,0 +1,30 @@
+
+///Index Array...
+
+let marks = [40, 50, 75, 35, 21];
+
+console.log (marks[0]);
+console.log (marks[4]);
+console.log (marks[3]);
+
+//Two Dimensional Array....
+
+let twoDimensionalArray = [ //Camel Case
+    ['Rahim', 'Karim', 'Bablu', 'Abdul Jobbar'],
+    ['Rubel', 'Shahin', 'Arif', 'Seyam'],
+    ['Utpol', 'Manik', 'Maruf', 'Mou'],
+ ]
+
+ console.log (twoDimensionalArray[2][2]);
+ console.log (twoDimensionalArray[1][1]);
+
+ //Associative Array...
+ let Bablu = {
+    firstName: 'Abdul Karim', age: 23, accountBalance: 426547
+ }
+
+ console.log (Bablu.age);
+ console.log (Bablu.accountBalance);
+
+ var sentence = 'My name is Muntasir';
+ console .log (sentence);
